11ec575156360fbcc46c027ea375376d01b10957
Zheng <zheng022@github.com>
2024-02-18 15:03:57 +0100

    Initial Commit

---
+++ GCalendar2/GCalendar2App.swift
@@ -0,0 +1,17 @@
+//
+//  GCalendar2App.swift
+//  GCalendar2
+//
+//  Created by Zheng on 18.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GCalendar2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
